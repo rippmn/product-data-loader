65a62d7e5413190ac6db676e953e90649b557956
rippmn <rippmn@gmail.com>
2017-12-11 15:18:20 -0600

    initial check in

---
+++ src/main/java/com/rippmn/product/web/ProductLoaderController.java
@@ -0,0 +1,22 @@
+package com.rippmn.product.web;
+
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductLoaderController {
+
+	private static Logger log = LoggerFactory.getLogger(ProductLoaderController.class);
+	
+	@RequestMapping("/loadProducts")
+	public String loadProducts() {
+		log.debug("returning homepage");
+		return "completed at:" + new Date().toString();
+	
+	}
+
+}
